8dda6e994c1084e3a52560d275a3e5de4d86104a
Itay-Atun <itay14717@gmail.com>
2024-08-31 16:02:18 +0300

    Before Functions

--- ytdownload.py
+++ ytdownload.py
@@ -8,17 +8,18 @@ import os
 ssl._create_default_https_context = ssl._create_stdlib_context
 musicFile = open("./music.txt", "r")
 
-function downloadFromYoutube():
-  for line in musicFile:
-    videoURL = YouTube(line)
-    print(f"downloading {videoURL.title}")
-    streamVid = videoURL.streams.filter(file_extension="mp4").first().download("./playlist")
+for line in musicFile:
+  videoURL = YouTube(line)
+  print(f"downloading {videoURL.title}")
+  streamVid = videoURL.streams.filter(file_extension="mp4").first().download("./playlist")
 
 
-mylist = os.listdir("/Users/itayat/Learning/GitHub-Projects/Youtube-Downloader/playlist")
+mylist = os.listdir("/Users/itayat/Learning/GitHub-Projects/Youtube-Downloader/playlist/")
+print (mylist)
 for fileName in mylist:
-    print(f"Downloading {fileName}")
-    video = VideoFileClip(fileName)
+    print(f"Converting {fileName}")
+    video = VideoFileClip(f"./playlist/{fileName}")
     baseName = fileName.rsplit(".mp4")
-    # print(baseName[0])
-    video.audio.write_audiofile(f".././converted/{baseName[0]}.mp3")
+    video.audio.write_audiofile(f"converted/{baseName[0]}.mp3")
+
+    
